811a28b0ba9549186675fbf0f3d0eb6774fdbf5b
Brian J. Miller <brian.miller@rusticisoftware.com>
2016-08-24 15:36:19 -0500

    Bump dependency versions to their latest
    * Add badges to README and other updates

--- src/LanguageMap.php
+++ src/LanguageMap.php
@@ -20,11 +20,11 @@ namespace TinCan;
 class LanguageMap extends Map
 {
     public function getNegotiatedLanguageString ($acceptLanguage = null) {
-        $negotiator = new \Negotiation\Negotiator();
+        $negotiator = new \Negotiation\LanguageNegotiator();
         if ($acceptLanguage === null) {
             $acceptLanguage = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE']. ', *' : '*';
         }
-        $availableLanguages = array_keys ($this->_map);
+        $availableLanguages = array_keys($this->_map);
         $preferredLanguage = $negotiator->getBest($acceptLanguage, $availableLanguages);
 
         return $this->_map[$preferredLanguage->getValue()];
